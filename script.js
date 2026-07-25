67d338306c1d9d343ffe58d6e18c82911543ff29
LudvigDahlberg <luddedahlberg@gmail.com>
2026-07-25 16:19:54 +0200

    started with objects

---
+++ script.js
@@ -0,0 +1,15 @@
+const players = [
+    {id: number, name: string, age: number, position: string, club: string, nationality: string, value: number}
+];
+
+players.push({
+  id: 1,
+  name: "Erik",
+  age: 24,
+  position: "Midfielder",
+  club: "AIK",
+  nationality: "Sverige",
+  value: 5000000
+});
+console.log(players);
+
